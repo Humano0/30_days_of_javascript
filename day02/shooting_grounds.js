125e767c368657805ba89bc9ec3f0c9353f89fed
Eyüp Can Yönter <oniraca.om00@gmail.com>
2022-10-23 13:18:11 +0300

    Made changes.

---
+++ day02/shooting_grounds.js
@@ -0,0 +1,37 @@
+let a = 3
+let b = 2
+console.log(`Sum of ${a} and ${b} is ${a + b}`)
+
+
+
+let string = '30 Days Of JavaScript'
+
+console.log(string.split())
+console.log(string.split(' '))
+console.log(string.split(''))
+
+console.log(string)
+console.log(string.trim(' '))
+
+console.log(string.startsWith("3" || "Days"))
+
+console.log(string.endsWith("Script"))
+
+console.log(string.search("Of"))
+
+console.log(string.match("JavaScript"))
+
+console.log(string.repeat(5))
+
+console.log(string.length)
+
+console.log(string.toUpperCase())
+console.log(string.toLowerCase())
+
+console.log(string.includes("ava"))
+
+console.log("The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.")
+console.log("\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.\"")
+
+let hello = "whaaat"
+console.log(`${hello} da hack`)
