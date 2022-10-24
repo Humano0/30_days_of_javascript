91f3be2653b9f0c24e93c079813ddc51e9bd50ae
Eyüp Can Yönter <oniraca.om00@gmail.com>
2022-10-24 22:28:07 +0300

    Finished day 4.

---
+++ day04/conditions.js
@@ -0,0 +1,15 @@
+let num = -3
+if (num > 0) {
+    console.log(`${num} is a positive number`)
+}
+else {
+    console.log(`${num} is a negative number`)
+}
+
+let isRaining = true
+if (isRaining) {
+    console.log('Remember to take your rain coat.')
+}
+else {
+    console.log('No need for a rain coat.')
+}
